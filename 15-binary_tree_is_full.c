bc9da7c37bd3b31aebd032c491ef53a583eb5ebb
pasebukky <olubukunolapase@gmail.com>
2024-01-29 16:30:34 +0100

    Update code usinf if statement

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,5 +19,8 @@ if (tree->right == NULL && tree->left == NULL)
 if (tree->right == NULL || tree->left == NULL)
 	return (0);
 
-return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
+	return (1);
+else
+	return (0);
 }
