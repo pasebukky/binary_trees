73aba135cc98733d157ca56035c0b5f3e6eafec6
pasebukky <olubukunolapase@gmail.com>
2024-01-30 12:44:49 +0100

    checker for root

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,10 +12,10 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-binary_tree_t *grandparent = node->parent->parent;
-
-if (node == NULL || node->parent == NULL || grandparent == NULL)
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	return (NULL);
+
+binary_tree_t *grandparent = node->parent->parent;
 
 if (grandparent == NULL)
 	return (NULL);
