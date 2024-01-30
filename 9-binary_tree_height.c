6eae1eb1f3f9cead11222f9ca241049b59963dfa
pasebukky <olubukunolapase@gmail.com>
2024-01-30 10:32:38 +0100

    Update code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,8 +9,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height = 0;
-size_t right_height = 0;
+size_t left_height;
+size_t right_height;
 
 if (tree == NULL)
 	return (0);
