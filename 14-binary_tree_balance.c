c9794ea6e810bee6b427b50133bdc5178790ff95
pasebukky <olubukunolapase@gmail.com>
2024-01-30 11:33:40 +0100

    update height code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,17 +10,25 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree == NULL)
-	return (0);
-
 size_t left_height;
 size_t right_height;
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
+if (tree == NULL)
+	return (0);
+
+if (tree->left != NULL)
+	left_height = (int)binary_tree_height(tree->left);
+else
+	left_height = -1;
+
+if (tree->right != NULL)
+	right_height = (int)binary_tree_height(tree->right);
+else
+	right_height = -1;
 
 return ((int)(left_height - right_height));
 }
+
 
 
 /**
@@ -30,19 +38,24 @@ return ((int)(left_height - right_height));
  *
  * Return: Tree height. If tree is NULL, your function must return 0
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (tree == NULL)
-	return (0);
-
 size_t left_height;
 size_t right_height;
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
+if (tree == NULL)
+	return (0);
 
-if (left_height > right_height)
-	return (left_height + 1);
+if (tree->left)
+	left_height = 1 + binary_tree_height(tree->left);
 else
-	return (right_height + 1);
+	left_height = 0;
+
+if (tree->right)
+	right_height = 1 + binary_tree_height(tree->right);
+else
+	right_height = 0;
+
+return (left_height > right_height ? left_height : right_height);
 }
