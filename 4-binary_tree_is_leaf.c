43d908074f11095ed9b6688f10986ca1bda5e250
pasebukky <olubukunolapase@gmail.com>
2024-01-30 10:22:53 +0100

    tree leaf update

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,10 +10,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-if (node->left == NULL && node->right == NULL)
-	return (1);
-else if (node == NULL)
+if (node == NULL || node->left != NULL || node->right != NULL)
 	return (0);
 else
-	return (0);
+	return (1);
 }
