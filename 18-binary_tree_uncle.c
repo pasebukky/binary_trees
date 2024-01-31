3d56f2d9fdfff1798660ef740464ed8f7a978da4
pasebukky <olubukunolapase@gmail.com>
2024-01-31 09:25:11 +0100

    Use of sibling to find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,15 +12,33 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-binary_tree_t *grandparent = node->parent->parent;
-
-if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+if (node == NULL || node->parent == NULL)
 	return (NULL);
 
-if (grandparent == NULL)
+return (binary_tree_sibling(node->parent));
+}
+
+
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ *
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: pointer to the sibling node.
+ * If node is NULL or the parent is NULL, return NULL
+ * If node has no sibling, return NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL)
 	return (NULL);
 
-if (grandparent->left == node->parent)
-	return (grandparent->right);
-return (grandparent->left);
+if (node->parent->left == node)
+	return (node->parent->right);
+else if (node->parent->right == node)
+	return (node->parent->left);
+else
+	return (NULL);
 }
